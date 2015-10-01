1ead9a51fca3f67f1f6bad20004d7dd70e4bc89e
Stefan Ingesson <stefan.ingesson@gmail.com>
2015-10-01 14:24:14 +0200

    Specialtecken test

--- Tester/Users.cs
+++ Tester/Users.cs
@@ -11,16 +11,35 @@ namespace Tester
         public string Name { get; set; }
         public string PassWord { get; set; }
 
-       //Första test
+
+       public void Add_User()
+        {
+            List<Users> user = new List<Users>();
+            //user.Add(new Users() { Name = "Rofl", PassWord = "Copter" });
+            if (Name.All(char.IsLetterOrDigit))
+            {
+                user.Add(new Users());
+            }
+            else
+            {
+                throw new FormatException();
+            }
+        }
+
+
+
+
+        #region Gammalt skit
+        //Första test
        //public string Name = "Abdullah";
        //public string PassWord = "Moreno";
 
-       public List<Users> user = new List<Users>();
+       //public List<Users> user = new List<Users>();
 
-       public void Add(Users users)
-       {
-           user.Add(new Users() { Name = "Rofl", PassWord = "Copter" });
-       }
+       //public void Add(Users users)
+       //{
+       //    user.Add(new Users() { Name = "Rofl", PassWord = "Copter" });
+       //}
 
         //List<Users> _users = new List<Users>();
 
@@ -33,7 +52,7 @@ namespace Tester
 
         //    _users.Add(user);
         //}
-
+        #endregion
 
     }
 
--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using Tester;
+using System.Linq;
 
 namespace UnitTestProject
 {
@@ -13,27 +14,30 @@ namespace UnitTestProject
         [TestMethod]
         public void Control_That_Name_And_Password_Is_Viable_Return_True()
         {
-            //Assert 
-           //var user = new Tester.Users();
-            //var name = user.Name;
-            
+        
+           //Arrange
           Users t = new Users();
           t.Name = "Abdullah";
           t.PassWord = "Moreno";
           var name = t.Name;
           var password = t.PassWord;
+
+            //Assert
           Assert.IsTrue("Abdullah" == name && "Moreno" == password);
         }
 
         [TestMethod]
         public void Control_That_Name_And_Password_Is_Viable_Return_False()
         {
+            //Arrange
             Users t = new Users();
             t.Name = "Abdullah";
             t.PassWord = "Moreno";
             var name = t.Name;
             var password = t.PassWord;
-            Assert.IsFalse("Fakename" == name && "FakeLastName" == password);
+
+            //Assert
+            Assert.IsFalse("Fakename" == name && "FakePassword" == password);
         }
 
 
@@ -44,14 +48,38 @@ namespace UnitTestProject
         {
             //Arrange - Skapa en lista med användare
             List<Users> user = new List<Users>();
-       
-            
+              
             //Act - Lägg till en användare i listan
-            user.Add(new Users() { Name = "Rofl", PassWord = "Copter" });
+            user.Add(new Users());
 
             //Assert - Kolla om listan har lagt till en användare
-           Assert.IsTrue(user.Count >= 1);
-          
+           Assert.IsTrue(user.Count >= 1);          
+        }
+
+
+
+
+        //Specialtecknen @, #, $, eller åäö är inte giltiga i användarnamn.
+        [TestMethod]
+        public void No_Special_Characters_Allowed_In_User_Name()
+        {
+            //Arrange
+            List<Users> user = new List<Users>();
+                    
+            //Act
+            user.Add(new Users());
+            string Name = "Bengt";
+               if (Name.All(char.IsLetterOrDigit))
+            {
+                user.Add(new Users());
+            }
+               else
+               {
+                   throw new FormatException();
+               }
+
+            //Assert
+               Assert.IsFalse(Name.Contains("@#$åäö"));
         }
     }
 }
